16d0ab46b336f1e757ca64442d68d209bf7f3c69
Jai Parkash <parkashj05@gmail.com>
2023-03-15 23:11:04 +0530

    Get in touch section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import About from '@/components/About'
+import Contact from '@/components/Contact'
 import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
 import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
 import Head from 'next/head'
-import Image from 'next/image'
 
 
 export default function Home() {
@@ -21,6 +21,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </>
   )
 }
